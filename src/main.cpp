841a63ca6ef07d55066078c6d6ebfb8d86925993
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-15 08:10:21 +0100

    Sensor now measures PPM

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <MQ135.h>
 
-void setup() {
-  
+#define MQ_PIN A0
+
+MQ135 mq135_sensor(MQ_PIN);
+
+
+float temp = 24.8;      // Must assume/get current temp
+float humidity = 30.0;  // Must use the current hum or assume it
+
+
+/*
+*       SENSOR VARIABLES
+*/
+
+float resistance_zero   = 0;
+float corrected_zero    = 0;
+float resistance        = 0;
+float ppm_measured      = 0;
+float ppm_corrected     = 0;
+
+void setup()
+{
+        Serial.begin(11500);
 }
 
-void loop() {
-  
+void loop()
+{
+        resistance_zero = mq135_sensor.getRZero();
+        corrected_zero  = mq135_sensor.getCorrectedResistance(temp, humidity);
+        resistance      = mq135_sensor.getResistance();
+        ppm_measured    = mq135_sensor.getPPM();
+        ppm_corrected   = mq135_sensor.getCorrectedPPM(temp, humidity);
+
+        Serial.println(ppm_corrected);
+        delay(10000);
 }
